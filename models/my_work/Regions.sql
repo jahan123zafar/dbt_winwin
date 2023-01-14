e81c4653562e70b4eba3cbfa8ae3a6ec29a8e88e
Jahan Zaib Zafar <jahanzaibcapricon@gmail.com>
2023-01-14 16:27:19 +0000

    Region updated

--- models/my_work/Regions.sql
+++ models/my_work/Regions.sql
@@ -1,10 +1,17 @@
 {{ config(materialized='table') }}
 
-with Regions as (
+with source_data as (
 
     SELECT * FROM interview-task-winwinproptech.winwin_db.Regions
+    LEFT JOIN(
+        SELECT Delivery_Region_Index as Index, Order_Quantity, Line_total, Customer_Name_Index, OrderDate
+        FROM interview-task-winwinproptech.winwin_db.Sales_Orders
+    )l 
+    USING(Index)
 
 )
 
-select *
-from Regions
+select  Index as region_Index, City, Territory, ROUND(SUM(Line_total),2) as total_sold, count(distinct Customer_Name_Index) as cnt_distinct_customers
+from source_data
+GROUP BY region_Index, City, Territory
+ORDER BY total_sold DESC
